d6c4e7230a34165ef084630163c5a71bfa08c756
philchurch77 <philchurch77@gmail.com>
2025-10-20 12:22:43 +0100

    Update wsgi.py

--- myproject/myprojectsettings/wsgi.py
+++ myproject/myprojectsettings/wsgi.py
@@ -2,5 +2,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myprojectsettings.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.myprojectsettings.settings")
 application = get_wsgi_application()
