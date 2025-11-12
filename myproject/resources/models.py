a8186b7413cedc12828365d021e67aad8ad0ac95
Phil <philchurch77@gmail.com>
2025-11-12 10:17:03 +0000

    Text Field

--- myproject/resources/models.py
+++ myproject/resources/models.py
@@ -5,7 +5,7 @@ from taggit.managers import TaggableManager
 
 class Topic(models.Model):
     title = models.CharField(max_length=200)
-    body = models.TextField(blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=200, unique=True, blank=False, null=False)
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
